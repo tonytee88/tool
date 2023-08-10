0a77742be416c643095a4aac1d1a4767784c6acd
Tony Tran <ttcomm45@gmail.com>
2023-08-10 11:17:16 -0400

    untrack sensible docs

--- api_asana/app.py
+++
@@ -1,9 +0,0 @@
-import asana
-
-client = asana.Client.access_token('1/1200208858117092:170a26a4a74a3421946fa26da29b3ade')
-
-workspacecode = "456872174481206"
-
-result = client.tasks.create_task({"workspace": workspacecode,"name":"TESTAPI3","notes":"ceci est écrit par un robot"}, opt_pretty=True)
-
-
