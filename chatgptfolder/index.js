55086cdf433ca1be76e77802759197e05fa8d193
Tony Tran <ttcomm45@gmail.com>
2023-08-15 15:15:12 -0400

    New js files for bigger updates

--- chatgptfolder/index.js
+++ chatgptfolder/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from a directory
-app.use('/static', express.static('chatgpt/javascript'));
+app.use('/static', express.static('chatgpt'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
