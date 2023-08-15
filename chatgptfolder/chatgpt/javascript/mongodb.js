79b04b0254fe550946dd109ee05f2263883f8711
Tony Tran <ttcomm45@gmail.com>
2023-08-15 10:23:50 -0400

    mongo raw file fix

--- chatgptfolder/chatgpt/javascript/mongodb.js
+++ chatgptfolder/chatgpt/javascript/mongodb.js
@@ -1,4 +1,4 @@
-console.log("raw js file via jsdelvr works!");
+console.log("raw mongo file via jsdelvr works!");
 var clientTraits2;
 var foundOneData;
 var clientName = document.getElementById("clients").value;
