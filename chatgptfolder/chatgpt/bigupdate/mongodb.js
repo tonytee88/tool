10401a6cb0255f0af74dc9d8225b1c0570623872
Tony Tran <ttcomm45@gmail.com>
2023-08-21 13:49:41 -0400

    fix client creation bug

--- chatgptfolder/chatgpt/bigupdate/mongodb.js
+++ chatgptfolder/chatgpt/bigupdate/mongodb.js
@@ -234,6 +234,10 @@ handleCreateMongoClick.addEventListener("click", async function() {
       step6.style.display = 'none';
       step1.style.display = 'block';
       currentSection = 1;
+      document.getElementById('goBackButton').disabled = true;
+      document.getElementById('goNextButton').disabled = true;
+      document.getElementById('goBackButton').style.backgroundColor = '#f1f1f1';
+      document.getElementById('goNextButton').style.backgroundColor = '#3498db';
   } catch (error) {
       console.log("Error:", error);
   }
