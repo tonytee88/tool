c7be013c355c20f85f96fd0baab83fe0c5a0feea
Tony Tran <ttcomm45@gmail.com>
2023-09-26 16:07:16 -0400

    gitingore

--- chatgptfolder/chatgpt/newfeatures/j7magicjavascript.js
+++ chatgptfolder/chatgpt/newfeatures/j7magicjavascript.js
@@ -1,12 +1,19 @@
+//page unit
+  // Call the function for each subcontainer
+  createStarContainer('descriptionContainer');
+  createStarContainer('linkedInContainer');
+  createStarContainer('newsletterContainer');
+  
 
-async function callApi(transcript, transcriptPrompt) {
+
+async function callApi(transcript, prompt) {
     try {
         const response = await fetch('https://j7-magic-tool.vercel.app/api/openaiCall', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ transcript, transcriptPrompt  }),
+            body: JSON.stringify({ transcript, prompt }),
         });
 
         if (!response.ok) {
@@ -28,10 +35,94 @@ async function callApi(transcript, transcriptPrompt) {
 }
 
 
-function submitForm() {
+function submitFormDescription() {
     const transcript = document.getElementById('transcript').value;
-    const transcriptPrompt = document.getElementById('promptTranscript').value; 
-    callApi(transcript, transcriptPrompt).then(response => {
-        document.getElementById('output').value = response;
+    const prompt = document.getElementById('descriptionPrompt').value; 
+    callApi(transcript, prompt).then(response => {
+        document.getElementById('descriptionOutput').value = response;
     });
 }
+
+function submitFormLinkedIn() {
+    const transcript = document.getElementById('transcript').value;
+    const prompt = document.getElementById('linkedInPrompt').value; 
+    callApi(transcript, prompt).then(response => {
+        document.getElementById('linkedInOutput').value = response;
+    });
+}
+
+function submitFormNewsletter() {
+    const transcript = document.getElementById('transcript').value;
+    const prompt = document.getElementById('newsletterPrompt').value; 
+    callApi(transcript, prompt).then(response => {
+        document.getElementById('newsletterOutput').value = response;
+    });
+}
+
+function createStarContainer(containerId) {
+    // Get the container element by ID
+    const container = document.getElementById(containerId);
+    
+    // Create the star container div and assign an ID
+    const starContainer = document.createElement('div');
+    starContainer.id = containerId + 'StarContainer';
+    
+    // Create a table to organize stars in 2 columns
+    const table = document.createElement('table');
+    for (let i = 0; i < 5; i++) {
+      const row = document.createElement('tr');
+      for (let j = 0; j < 2; j++) {
+        const cell = document.createElement('td');
+        cell.textContent = '☆'; // Unicode for star
+        row.appendChild(cell);
+      }
+      table.appendChild(row);
+    }
+    // ★
+    // Append the table to the star container
+    starContainer.appendChild(table);
+    
+    // Append the star container to the main container
+    container.appendChild(starContainer);
+  }
+  
+  function getStarCountFromMongoDB(category) {
+    const apiKey = getMongoApiKey();
+    var statusLog = "Start of mongo api call...\n";
+    const url = 'https://us-east-1.aws.data.mongodb-api.com/app/data-gkvfy/endpoint/data/v1/action/findOne';
+    
+    const data = JSON.stringify({
+      "collection": "clientsCollection",
+      "database": "clientsDB",
+      "dataSource": "Cluster0",
+      "filter": {
+        "name": clientName
+      },
+      "projection": {
+        "_id": 1,
+        "name": 1,
+        "traits": 1,
+        "upvotes":1,
+        "downvotes":1
+      }
+    });
+    statusLog += "Data:" + data +"\n";
+    const config = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': apiKey
+      },
+      payload: data
+    };
+  
+    const response = UrlFetchApp.fetch(url, config);
+    const responseData = JSON.parse(response.getContentText());
+  
+    statusLog += "responseData:" + responseData +"\n";
+    // Process the response data as needed
+  
+    //return {result: responseData, statusLog: statusLog};
+    return responseData
+  }
