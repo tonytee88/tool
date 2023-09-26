b36139ed390962f28bde7c9ddb32cf5419726e2a
Tony Tran <ttcomm45@gmail.com>
2023-09-26 12:21:08 -0400

    css fix

--- chatgptfolder/chatgpt/newfeatures/j7magicjavascript.js
+++ chatgptfolder/chatgpt/newfeatures/j7magicjavascript.js
@@ -1,12 +1,12 @@
-// client-side code
-async function callApi(prompt) {
+
+async function callApi(transcript, transcriptPrompt) {
     try {
         const response = await fetch('https://j7-magic-tool.vercel.app/api/openaiCall', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ prompt }),
+            body: JSON.stringify({ transcript, transcriptPrompt  }),
         });
 
         if (!response.ok) {
@@ -14,17 +14,24 @@ async function callApi(prompt) {
         }
 
         const data = await response.json();
-        console.log(data); // Process the response data as needed
-        return data; // Return the data
+        //console.log(data); // Process the response data as needed
+        const content = data.choices && data.choices[0] && data.choices[0].message && data.choices[0].message.content;
+        // Check if content is undefined and return an error message if it is
+        if (content === undefined) {
+            return "Error: Content is undefined";
+        }
+
+        return content;
     } catch (error) {
         console.error('Error calling the API', error);
     }
 }
 
 
-  function submitForm() {
-    const transcript = document.getElementById('transcript').value; // Get the value from the transcript textarea
-    callApi(transcript).then(response => {
-        document.getElementById('output').value = JSON.stringify(response, null, 2); // Display the API response in the output textarea
+function submitForm() {
+    const transcript = document.getElementById('transcript').value;
+    const transcriptPrompt = document.getElementById('promptTranscript').value; 
+    callApi(transcript, transcriptPrompt).then(response => {
+        document.getElementById('output').value = response;
     });
 }
