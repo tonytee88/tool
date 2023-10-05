124240b32352280cd9eaebabe811c5709326303f
Tony Tran <ttcomm45@gmail.com>
2023-10-04 22:04:06 -0400

    idea list works

--- chatgptfolder/chatgpt/newfeatures/treescript.js
+++ chatgptfolder/chatgpt/newfeatures/treescript.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     incrementCategory();
     initTrees();
+    getAndLoadIdeas();
 });
 
 function showGarden() {
@@ -113,7 +114,7 @@ async function treeMongoGetCount(category) {
         }
 
         const data = await response.json();
-        console.log(data)
+        //console.log(data)
         // Check if the count property is available in the nested document object
         if (data.document && data.document.count !== undefined) {
             return data.document.count;
@@ -124,5 +125,112 @@ async function treeMongoGetCount(category) {
     } catch (error) {
         console.error('Error calling the API', error);
         return 0;  // You may choose to return 0 or any other default value in case of error
+    }
+}
+
+const submitButton = document.getElementById('submitIdea');
+submitButton.addEventListener('click', async () => {
+    const idea = document.getElementById('idea').value;
+    const category = document.getElementById('categoryDropdownForIdeas').value;
+
+    if (idea.trim() !== "") {
+        await treeMongoAddNotebookIdeas(category, idea);
+        document.getElementById('idea').value = '';  // Clear the input
+        getAndLoadIdeas();
+    } else {
+        alert("Please enter a note!");
+    }
+});
+
+async function treeMongoAddNotebookIdeas(category, idea) {
+    try {
+        const response = await fetch('https://j7-magic-tool.vercel.app/api/treeMongoAddNotebookIdeas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ category, idea }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        //console.log(data); // Process the response data as needed
+        const content = data;
+
+        // Check if content is undefined and return an error message if it is
+        if (content === undefined) {
+            return "Error: Content is undefined";
+        }
+
+        return content;
+    } catch (error) {
+        console.error('Error calling the API', error);
+    }
+}
+
+async function getAndLoadIdeas() {
+    const ideasList = document.getElementById('ideasList');
+
+    // Clear previous ideas
+    ideasList.innerHTML = '';
+
+    for (const category of Object.keys(categoryColors)) {
+        console.log("fetching category from category colors"+category)
+        const ideas = await treeMongoFetchIdeas(category);
+
+        // Filter out empty ideas
+        const filteredIdeas = ideas.filter(idea => idea.trim() !== "");
+        
+        filteredIdeas.forEach(idea => {
+            const ideaTag = document.createElement('div');
+            ideaTag.className = 'ideaTag';
+            ideaTag.style.backgroundColor = categoryColors[category];
+            ideaTag.innerText = idea;
+            ideasList.appendChild(ideaTag);
+        });
+    }
+}
+
+const categoryColors = {
+    "Husband": "#f28d35",  // Example color
+    "Fatherhood": "#35a2f2",
+    "Home": "#9ef235",
+    "Money": "#e835f2",
+    "Brain": "#35f2a2",
+    "Body": "#f23572",
+    // ... Add more categories and colors if needed
+};
+
+async function treeMongoFetchIdeas(category) {
+    console.log("treeMongoFetchIdeas" + category);
+    try {
+        const response = await fetch('https://j7-magic-tool.vercel.app/api/treeMongoFetchIdeas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ category }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        // Check if the ideas property is available in the nested document object
+        if (data.document && Array.isArray(data.document.ideas)) {
+            console.log(data.document.ideas);
+            return data.document.ideas;
+        } else {
+            console.error('Ideas array not found in the response data');
+            return [];  // Return an empty array if ideas are not found
+        }
+    } catch (error) {
+        console.error('Error calling the API', error);
+        return [];  // Return an empty array in case of error
     }
 }
